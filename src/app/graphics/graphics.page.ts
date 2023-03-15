ab608d55d8596bebaef96a05eecebbb4c292915a
HectorRuizMX <hector.omar@upnify.cc>
2023-03-14 19:02:05 -0500

    Database connection added

--- src/app/graphics/graphics.page.ts
+++ src/app/graphics/graphics.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import { RealtimeDatabaseService } from '../services/realtime-database.service';
 
 @Component({
   selector: 'app-graphics',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphicsPage implements OnInit {
 
-  constructor() { }
+  data1: any;
+  data: any;
+  data2: any;
+
+  constructor(private dataService: RealtimeDatabaseService) { }
 
   ngOnInit() {
+    this.dataService.getData().subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    });
+    this.dataService.leerDatos('Comedero/Almacen').subscribe(data1 => {
+      this.data1 = data1;
+      console.log(this.data1);
+    });
+    this.dataService.leerDatos('Comedero/Fotos').subscribe(data2 => {
+      this.data2 = data2;
+      console.log(this.data2);
+    });
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  //Your web app's Firebase configuration
+  firebaseConfig: {
+  apiKey: "AIzaSyAwX_-bZr8g18NMHuJbTRqbNzzaJfk2L4k",
+  authDomain: "blue-jay-e4f63.firebaseapp.com",
+  databaseURL: "https://blue-jay-e4f63-default-rtdb.firebaseio.com",
+  projectId: "blue-jay-e4f63",
+  storageBucket: "blue-jay-e4f63.appspot.com",
+  messagingSenderId: "63567735733",
+  appId: "1:63567735733:web:65912b6b545456045e87fc"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
